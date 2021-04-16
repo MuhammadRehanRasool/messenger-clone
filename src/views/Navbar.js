9a29e6a2bfd48674a2a8138533ca7ffb7eb8d9c5
Muhammad Rehan <muhammadrehanrasool@gmail.com>
2021-04-16 12:25:19 -0700

    Metatags and Stuff

--- src/views/Navbar.js
+++ src/views/Navbar.js
@@ -6,7 +6,7 @@ function Navbar() {
   return (
     <div className="Navbar">
     	<ReactTooltip/>
-	   	<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/be/Facebook_Messenger_logo_2020.svg/600px-Facebook_Messenger_logo_2020.svg.png" alt="Messenger Logo" className="logo"/>
+	   	<img src="/logo.png" alt="Messenger Logo" className="logo"/>
     	<h1 className="logo_written">Messenger Clone</h1>
     	<div id="github_reference" data-tip="@MuhammadRehanRasool" onClick={() => {
     		window.location = "https://github.com/MuhammadRehanRasool";
